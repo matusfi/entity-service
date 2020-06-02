ea8df0794e1fb55159c9b7b7847e3a1e55bb72d7
Matus Laslofi <matus.laslofi@gmail.com>
2020-06-02 12:20:43 +0200

    Fix up the BaseEntity constructor test

--- src/test/java/sk/laslofi/entityService/models/BaseEntityTest.java
+++ src/test/java/sk/laslofi/entityService/models/BaseEntityTest.java
@@ -2,10 +2,8 @@ package sk.laslofi.entityService.models;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -13,7 +11,7 @@ import static org.junit.Assert.assertNotNull;
 public class BaseEntityTest {
 
     Map<String, Object> makeEntityMap(String id, Object subEntities, Object data) {
-        Map<String,Object> entityMap = new HashMap<String, Object>();
+        Map<String,Object> entityMap = new HashMap<>();
         entityMap.put("id", id);
         entityMap.put("subEntities", subEntities);
         entityMap.put("data", data);
@@ -24,16 +22,26 @@ public class BaseEntityTest {
     public void mapConstructorTest() {
         Map<String,Object> subEnt1 = new HashMap<>();
 
-        Object subEntities = Arrays.asList(
-                makeEntityMap("id1", Collections.EMPTY_SET, Collections.EMPTY_MAP),
-                makeEntityMap("id2", Collections.EMPTY_SET, Collections.EMPTY_MAP)
+        List<Map<String, Object>> expectedSubEntities = Arrays.asList(
+                makeEntityMap("id1", Collections.EMPTY_LIST, Collections.EMPTY_MAP),
+                makeEntityMap("id2", Collections.EMPTY_LIST, Collections.EMPTY_MAP)
         );
 
-        Map<String,Object> entityMap = makeEntityMap("root-entity-id", subEntities, Collections.EMPTY_MAP);
+        Map<String,Object> entityMap = makeEntityMap("root-entity-id", expectedSubEntities, Collections.EMPTY_MAP);
         Entity entity = new BaseEntity(entityMap);
 
         assertNotNull(entity);
         assertEquals("root-entity-id", entity.getId());
-        assertEquals(2, entity.getSubEntities().size());
+
+        Set<Entity> actualSubEntities = entity.getSubEntities();
+        assertEquals(2, actualSubEntities.size());
+
+        Set<String> expectedSubEntitiesIds = expectedSubEntities.stream()
+                .map(em -> (String) em.get("id"))
+                .collect(Collectors.toSet());
+        Set<String> actualSubEntitiesIds = actualSubEntities.stream()
+                .map(Entity::getId).collect(Collectors.toSet());
+        assertEquals(expectedSubEntitiesIds, actualSubEntitiesIds);
+
     }
 }
--- src/test/java/sk/laslofi/entityService/models/BaseEntityTestTest.java
+++
@@ -1,7 +0,0 @@
-package sk.laslofi.entityService.models;
-
-import static org.junit.Assert.*;
-
-public class BaseEntityTestTest {
-
-}
